b6618b2fd9a25d5508c624912a80e382eef51618
engrnahiankhan <engrnahiankhan@gmail.com>
2025-10-31 17:54:06 +0600

    updated management

--- app/admin-dashboard/layout.tsx
+++ app/admin-dashboard/layout.tsx
@@ -1,7 +1,18 @@
 import DashboardLayout from "@/components/adminDashboard/dashboard-layout";
 import "@/styles/admin-dashboard.css";
+import { Toaster } from "@/components/ui/sonner";
 export default function AdminDashboardLayout({
   children,
 }: React.PropsWithChildren) {
-  return <DashboardLayout>{children}</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      {children}
+      <Toaster
+        position="top-right"
+        richColors={true}
+        theme="dark"
+        closeButton={true}
+      />
+    </DashboardLayout>
+  );
 }
