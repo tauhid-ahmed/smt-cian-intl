d2a4d3620aad63e3546ca6f4fcf16fad84e4d99d
engrnahiankhan <engrnahiankhan@gmail.com>
2025-10-31 19:59:41 +0600

    donation page initial setup done

--- app/admin-dashboard/donation/page.tsx
+++ app/admin-dashboard/donation/page.tsx
@@ -1,10 +1,49 @@
-"use client";
+import {
+  CustomTabs,
+  CustomTabsContent,
+  CustomTabsList,
+  CustomTabsTrigger,
+} from "@/components/adminDashboard/custom-tabs";
+import OverviewTab from "@/components/adminDashboard/donationManagement/OverviewTab";
+import DashboardPageHeader from "@/components/shared/DashboardPageHeader";
 
 const DonationManagementPage = () => {
   return (
-    <div>
-      <h1>aciud</h1>
-    </div>
+    <>
+      <DashboardPageHeader
+        title="Analytics & Insights"
+        sub_title="Monitor your business performance and customer behavior"
+      />
+
+      <div>
+        <CustomTabs defaultValue="tab1">
+          <CustomTabsList variant="bordered">
+            <CustomTabsTrigger value="tab1" variant="bordered">
+              Overview
+            </CustomTabsTrigger>
+            <CustomTabsTrigger value="tab2" variant="bordered">
+              Review Moderation
+            </CustomTabsTrigger>
+            <CustomTabsTrigger value="tab3" variant="bordered">
+              Tax Receipts
+            </CustomTabsTrigger>
+            <CustomTabsTrigger value="tab4" variant="bordered">
+              Donor CRM
+            </CustomTabsTrigger>
+          </CustomTabsList>
+
+          <CustomTabsContent value="tab1">
+            <OverviewTab />
+          </CustomTabsContent>
+
+          <CustomTabsContent value="tab2">adsvgdavbd</CustomTabsContent>
+
+          <CustomTabsContent value="tab3">sv dv</CustomTabsContent>
+
+          <CustomTabsContent value="tab4">dvdav</CustomTabsContent>
+        </CustomTabs>
+      </div>
+    </>
   );
 };
 
