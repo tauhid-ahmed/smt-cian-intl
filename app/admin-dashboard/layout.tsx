2dc7de8d8422a73c49ec3a1075bcb7f2e0abe53c
ibrahim0891 <nijhum0891@gmail.com>
2025-12-27 17:07:09 +0600

    feat: add logged in user info in admin dashaboard header

--- app/admin-dashboard/layout.tsx
+++ app/admin-dashboard/layout.tsx
@@ -2,18 +2,21 @@ import DashboardLayout from "@/components/adminDashboard/dashboard-layout";
 import "@/styles/admin-dashboard.css";
 import { Toaster } from "@/components/ui/sonner";
 import { Suspense } from "react";
+import AuthProvider from "@/features/auth/provider/AuthProvider";
 export default function AdminDashboardLayout({
-  children,
+    children,
 }: React.PropsWithChildren) {
-  return (
-    <DashboardLayout>
-      <Suspense>{children}</Suspense>
-      <Toaster
-        position="top-right"
-        richColors={true}
-        theme="dark"
-        closeButton={true}
-      />
-    </DashboardLayout>
-  );
+    return (
+        <AuthProvider>
+            <DashboardLayout>
+                <Suspense>{children}</Suspense>
+                <Toaster
+                    position="top-right"
+                    richColors={true}
+                    theme="dark"
+                    closeButton={true}
+                />
+            </DashboardLayout>
+        </AuthProvider>
+    );
 }
