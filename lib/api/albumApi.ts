9cd2d7851f2c5be8c4e9d1ee785cad77f9f530bb
ibrahim0891 <nijhum0891@gmail.com>
2026-01-06 09:54:46 +0600

    integrate api to update and delete an album

--- lib/api/albumApi.ts
+++ lib/api/albumApi.ts
@@ -2,62 +2,62 @@ import { API_ENDPOINTS } from "../config/api";
 import { baseApi } from "./baseApi";
 
 export const adminApi = baseApi.injectEndpoints({
-  endpoints: (builder) => ({
-    // ADD
-    addAlbum: builder.mutation<any, FormData>({
-      query: (formData) => ({
-        url: API_ENDPOINTS.ALBUM.ADD_ALBUM,
-        method: "POST",
-        body: formData,
-      }),
-      invalidatesTags: ["Albums"],
-    }),
+    endpoints: (builder) => ({
+        // ADD
+        addAlbum: builder.mutation<any, FormData>({
+            query: (formData) => ({
+                url: API_ENDPOINTS.ALBUM.ADD_ALBUM,
+                method: "POST",
+                body: formData,
+            }),
+            invalidatesTags: ["Albums"],
+        }),
 
-    // UPDATE
-    updateAlbum: builder.mutation<any, FormData>({
-      query: (formData) => ({
-        url: API_ENDPOINTS.ALBUM.UPDATE_ALBUM,
-        method: "PATCH",
-        body: formData,
-      }),
-      invalidatesTags: ["Albums"],
-    }),
+        // UPDATE
+        updateAlbum: builder.mutation<any, { id: string; formData: FormData }>({
+            query: ({ id, formData }) => ({
+                url: `${API_ENDPOINTS.ALBUM.UPDATE_ALBUM}/${id}`,
+                method: "PATCH",
+                body: formData,
+            }),
+            invalidatesTags: ["Albums"],
+        }),
 
-    // GET ALL
-    getAllAlbums: builder.query<any, void>({
-      query: () => ({
-        url: API_ENDPOINTS.ALBUM.GET_ALL_ALBUM,
-        method: "GET",
-      }),
-      providesTags: ["Albums"],
-    }),
+        // GET ALL
+        getAllAlbums: builder.query<any, void>({
+            query: () => ({
+                url: API_ENDPOINTS.ALBUM.GET_ALL_ALBUM,
+                method: "GET",
+            }),
+            providesTags: ["Albums"],
+        }),
 
-    // GET SINGLE
-    getSingleAlbum: builder.query<any, string>({
-      query: (id) => ({
-        url: `${API_ENDPOINTS.ALBUM.GET_SINGLE_ALBUM}/${id}`,
-        method: "GET",
-      }),
-      providesTags: ["Albums"],
-    }),
+        // GET SINGLE
+        getSingleAlbum: builder.query<any, string>({
+            query: (id) => ({
+                url: `${API_ENDPOINTS.ALBUM.GET_SINGLE_ALBUM}/${id}`,
+                method: "GET",
+            }),
+            providesTags: ["Albums"],
+        }),
 
-    // DELETE
-    deleteAlbum: builder.mutation<any, string>({
-      query: (id) => ({
-        url: `${API_ENDPOINTS.ALBUM.DELETE_ALBUM}/${id}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: ["Albums"],
+        // DELETE
+        deleteAlbum: builder.mutation<any, string>({
+            query: (id) => ({
+                url: `${API_ENDPOINTS.ALBUM.DELETE_ALBUM}/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["Albums"],
+        }),
     }),
-  }),
-  overrideExisting: false,
+    overrideExisting: false,
 });
 
 export const {
-  useAddAlbumMutation,
-  useUpdateAlbumMutation,
-  useGetAllAlbumsQuery,
-  useGetSingleAlbumQuery,
-  useDeleteAlbumMutation,
-  useLazyGetAllAlbumsQuery,
+    useAddAlbumMutation,
+    useUpdateAlbumMutation,
+    useGetAllAlbumsQuery,
+    useGetSingleAlbumQuery,
+    useDeleteAlbumMutation,
+    useLazyGetAllAlbumsQuery,
 } = adminApi;
